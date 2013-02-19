26fb4e465821ed4c98d7055f9a1808c44f5d12db
varlogin <varlogin@gmail.com>
2013-02-19 10:40:00 +0900

    Смена версии в setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,7 @@
-
 from setuptools import setup, find_packages
 setup(
     name='django-simple-redis-captcha',
-    version='0.3.6',
+    version='0.3.7',
     description='A very simple, yet powerful, Django captcha application empowered with Redis',
     author='Marco Bonetti',
     author_email='mbonetti@gmail.com',
